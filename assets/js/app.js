e03756d7e48d924a68b019a284e58e686be9dc72
fernandagil <fernandagilgonzalez@gmail.com>
2020-12-09 18:32:14 +0000

    Added main functions

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+function runGame() {
+
+}
+
+// Called when the user clicks the Submit button or presses Enter
+
+function checkAnswer() {
+
+}
+
+function calculateRightAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+// Displays the questions.
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+    
+}
